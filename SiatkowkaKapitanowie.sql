9a25821231cd1724f2badd95027e7e107ec6893e
MateuszRL96 <101333426+MateuszRL96@users.noreply.github.com>
2023-01-10 23:13:32 +0100

    Kapitanowie

---
+++ SiatkowkaKapitanowie.sql
@@ -0,0 +1,21 @@
+/*pitanid int NOT NULL,
+	zawodnikid int NOT NULL,
+	telefon nchar(10) NOT NULL,
+	nazwaFirmy nvarchar(50) NOT NULL */
+
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (1, 4, '543745764', 'Zaklad Stolarki Sikora');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (2, 15, '531668956', 'Zaklad Fryzjerski Maja');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (3, 19, '683742567', 'Zaklad Murarski Mateja');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (4, 26, '784825144', 'Zaklad Pilkarski Ubert');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (5, 35, '674314674', 'Zaklad P³ytkarski Romero');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (6, 37, '59678565', 'Zaklad Kominiarski Jan');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (7, 52, '452545764', 'Zaklad Tynkarski Julia');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (8, 67, '854745764', 'Zaklad Papierniczy San');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (9, 78, '462245764', 'Zaklad Przetworniczy B');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (10, 89, '376245764', 'Zaklad Towarowy MinMar');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (11, 98, '543745764', 'Zaklad Tworzywa i Stal');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (12, 100, '543745764', 'Zaklad Manufakturowy C');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (13, 119, '597895764', 'Zaklad Stolarki Prymat');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (14, 124, '645221764', 'Zaklad Stolarki Rosman');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (15, 134, '645245764', 'Zaklad Kosmetologi Zen');
+INSERT INTO dbo.Kapitanowie(Kapitanid, zawodnikid, telefon, nazwaFirmy) Values (16, 147, '876546764', 'Zaklad Stolarki Jakub');
