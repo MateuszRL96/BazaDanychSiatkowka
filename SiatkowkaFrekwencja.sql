8630fed0650802988d0df8c8e7534568cfde6f1b
MateuszRL96 <101333426+MateuszRL96@users.noreply.github.com>
2023-01-10 23:13:09 +0100

    Frekwencja

---
+++ SiatkowkaFrekwencja.sql
@@ -0,0 +1,168 @@
+
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(1, 5, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(2, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(3, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(4, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(5, 1, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(6, 1, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(7, 3, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(8, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(9, 5, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(10, 3, 3, 2);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(11, 5, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(12, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(13, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(14, 2, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(15, 1, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(16, 1, 3, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(17, 3, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(18, 5, 1, 2);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(19, 3, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(20, 3, 3, 2);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(21, 3, 2, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(22, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(23, 5, 1, 2);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(24, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(25, 1, 2, 2);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(26, 1, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(27, 3, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(28, 2, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(29, 5, 2, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(30, 3, 3, 2);
+  
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(31, 5, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(32, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(33, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(34, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(35, 1, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(36, 1, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(37, 3, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(38, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(39, 5, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(40, 3, 3, 2);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(41, 5, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(42, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(43, 5, 1, 2);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(44, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(45, 1, 2, 3);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(46, 1, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(47, 6, 0, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(48, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(49, 5, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(50, 5, 3, 2);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(51, 5, 0, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(52, 2, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(53, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(54, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(55, 5, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(56, 1, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(57, 3, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(58, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(59, 5, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(60, 5, 3, 0);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(61, 5, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(62, 3, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(63, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(64, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(65, 1, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(66, 2, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(67, 3, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(68, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(69, 5, 2, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(70, 3, 3, 0);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(71, 5, 0, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(72, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(73, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(74, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(75, 1, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(76, 1, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(77, 3, 0, 2);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(78, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(79, 5, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(80, 3, 3, 2);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(81, 1, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(82, 2, 2, 2);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(83, 2, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(84, 4, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(85, 1, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(86, 1, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(87, 3, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(88, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(89, 4, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(90, 3, 3, 2);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(91, 5, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(92, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(93, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(94, 5, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(95, 1, 2, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(96, 4, 4, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(97, 3, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(98, 1, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(99, 5, 4, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(100, 3, 2, 2);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(101, 5, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(102, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(103, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(104, 5, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(105, 3, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(106, 3, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(107, 3, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(108, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(109, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(110, 3, 3, 0);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(111, 5, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(112, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(113, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(114, 2, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(115, 1, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(116, 1, 3, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(117, 3, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(118, 5, 1, 3);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(119, 0, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(120, 3, 3, 0);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(121, 3, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(122, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(123, 5, 1, 2);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(124, 5, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(125, 1, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(126, 2, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(127, 3, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(128, 2, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(129, 5, 2, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(130, 3, 3, 3);
+  
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(131, 5, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(132, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(133, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(134, 5, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(135, 1, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(136, 0, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(137, 3, 0, 3);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(138, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(139, 5, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(140, 3, 3, 2);
+
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(141, 5, 0, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(142, 2, 2, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(143, 4, 1, 2);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(144, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(145, 1, 2, 3);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(146, 1, 4, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(147, 6, 0, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(148, 5, 1, 0);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(149, 5, 1, 1);
+INSERT INTO dbo.Frekwencja(playerid, ileRazyByl, ileRazyNiebylo, wZastepstwie)VALUES(150, 5, 3, 2);
+
+select * from Frekwencja
