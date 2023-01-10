06b2901fb06eb5c8b02fa97db5098589bc38c067
MateuszRL96 <101333426+MateuszRL96@users.noreply.github.com>
2023-01-10 23:12:39 +0100

    Add files via upload
    Bilety

---
+++ SiatkowkaBilety.sql
@@ -0,0 +1,26 @@
+--SET IDENTITY_INSERT dbo.Bilety ON;
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (1, 83, '1', 'Czarny');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (2, 223, '1', 'Czerwoni');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (3, 1412, '3', 'Szary');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (4, 532, '3', 'Czerwoni');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (5, 526, '3', 'Czerwoni');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (6, 5346, '1', 'Czerwoni');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (7, 1231, '1', 'Pomoraczowy');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (8, 1260, '1', 'Pomoraczowy');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (9, 570, '1', 'Pomoraczowy');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (10, 970, '1', 'Pomoraczowy');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (11, 346, '1', 'Szary');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (12, 675, '3', 'Czerwoni');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (13, 230, '3', 'Czerwoni');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (14, 638, '3', 'Czerwoni');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (15, 8890, '3', 'Zielony');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (16, 1271, '1', 'Zielony');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (17, 3165, '1', 'Zielony');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (18, 678, '1', 'Szary');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (19, 246, '3', 'Szary');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (20, 9853, '3', 'Czerwoni');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (21, 90, '3', 'Czerwoni');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (22, 83, '2', 'Czarny');
+INSERT INTO dbo.Bilety(meczid, iloscBiletow, ochrona, NazwaOchrony) VALUES (23, 305, '2', 'Czarny');
+
+select * from Bilety
