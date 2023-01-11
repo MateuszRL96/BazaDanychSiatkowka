2eb55eae671001df5c7fb389e900ac5e7f4ef788
MateuszRL96 <101333426+MateuszRL96@users.noreply.github.com>
2023-01-11 09:32:20 +0100

    Platnosci

---
+++ SiatkowkaPlatnosci.sql
@@ -0,0 +1,26 @@
+/*
+druzynaid int NOT NULL,
+	platnosc nchar(10) NOT NULL,
+	kiedyzapacono date NOT NULL,
+	odetki int NOT NULL,
+	drugadata date NOT NULL
+	*/
+
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (1, 'zaplacono', '2022-10-15', 200, '2022-11-26');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (2, 'zaplacono', '2022-10-23', 140, '2022-11-17');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (3, 'zaplacono', '2022-10-16', 156, '2022-11-26');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (4, 'zaplacono', '2022-10-23', 123, '2022-11-30');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (5, 'zaplacono', '2022-10-12', 0, '2022-10-12');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (6, 'zaplacono', '2022-10-24', 0, '2022-10-24'); 
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (7, 'zaplacono', '2022-10-23', 0, '2022-10-23');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (8, 'zaplacono', '2022-09-12', 20, '2022-11-30');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (9, 'zaplacono', '2022-10-23', 500, '2022-11-25');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (10, 'zaplacono', '2022-10-23', 450, '2022-11-30');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (11, 'zaplacono', '2022-09-14', 128, '2022-11-24');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (12, 'zaplacono', '2022-10-23', 72, '2022-11-15');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (13, 'zaplacono', '2022-09-18', 200, '2022-11-28');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (14, 'zaplacono', '2022-10-23', 130, '2022-11-17');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (15, 'zaplacono', '2022-10-18', 140, '2022-11-28');
+INSERT INTO dbo.Platnosci(druzynaid, platnosc, kiedyzapacono, odetki, drugadata) values (16, 'zaplacono', '2022-10-29', 0, '2022-10-23');
+
+select * from Platnosci
