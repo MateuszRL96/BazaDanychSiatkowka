431e8498dcebec11561c0b202dcd1851aff0a759
MateuszRL96 <101333426+MateuszRL96@users.noreply.github.com>
2023-01-11 09:24:55 +0100

    Tabela

---
+++ SiatkowkaTabela.sql
@@ -0,0 +1,27 @@
+/*
+	druzynaid int NOT NULL,
+	punkty int NOT NULL,
+	wygrane int NOT NULL,
+	przegrane int NOT NULL
+	*/
+
+
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (1, 20, 6, 2);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (2, 19, 6, 2);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (3, 18, 6, 2);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (4, 17, 5, 3);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (5, 15, 4, 4);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (6, 14, 4, 4);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (7, 13, 4, 4);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (8, 11, 3, 5); 
+
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (9, 11, 3, 5);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (10, 10, 3, 5);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (11, 9, 3, 5);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (12, 8, 2, 6);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (13, 6, 2, 6);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (14, 6, 2, 6);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (15, 3, 1, 7);
+INSERT INTO dbo.tabela(druzynaid, punkty, wygrane, przegrane) values (16, 0, 0, 8);
+
+select * from tabela
